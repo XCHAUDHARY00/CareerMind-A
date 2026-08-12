dd665d46e04e29d9407ccd78ea6f6cbf89f01479
Raj <dhariwalraj37@gmail.com>
2026-08-12 12:30:59 +0530

    fix: add requirements.txt to root and backend, enable automatic server DATABASE_URL support with fallback

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,14 +83,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE_URL')
-    )
-}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
