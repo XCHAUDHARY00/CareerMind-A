83347d895a1fef4d8916dfbd3141eb3ef066be4a
Raj <dhariwalraj37@gmail.com>
2026-08-19 19:10:44 +0530

    fix(backend): remove all leftover references to django channels to fix vercel crash

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,11 +38,9 @@ ALLOWED_HOSTS = ['*', '.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'channels',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -85,12 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
-    },
-}
 
 
 DATABASE_URL = os.getenv('DATABASE_URL', '').strip().strip('\'"')
